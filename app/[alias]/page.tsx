ac982d95ff5b6c565cea76af58fafa3661b42c99
junoh-cho <cjo1003@gmail.com>
2025-08-04 18:17:38 -0400

    Save progress before running Next.js codemod

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,9 +1,17 @@
 import getCollection from '@/db';
 import { redirect, notFound } from 'next/navigation';
 
+
+type AliasRedirectProps = {
+    params: {
+        alias: string;
+    };
+};
+
+export default async function AliasRedirectPage({ params }: AliasRedirectProps) {
 //params.alias is == /[alias]
-export default async function AliasRedirectPage({ params }: { params: { alias: string } }) {
     const collection = await getCollection('urls');
+    console.log(typeof params);
     const result = await collection.findOne({ alias: params.alias });
 
     if (!result) {
